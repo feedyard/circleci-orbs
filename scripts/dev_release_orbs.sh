b6813ef4eae77d886b4caf5e9a9d3d0e62a250df
ncheneweth <nchenewe@thoughtworks.com>
2018-12-03 17:25:30 -0600

    iterating

---
+++ scripts/dev_release_orbs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eo pipefail
+
+for ORB in src/*; do
+  echo "Dev release $ORB ..."
+
+  circleci orb publish $CIRCLE_PROJECT_USERNAME/$ORB@dev:$CIRCLE_SHA1 --token $CIRCLECI_API_TOKEN; RETURN_CODE=$?
+  circleci orb publish $CIRCLE_PROJECT_USERNAME/$ORB@dev:latest --token $CIRCLECI_API_TOKEN; RETURN_CODE=$?
+
+  if [[ $RETURN_CODE != 0 ]]; then
+  	exit 1
+  fi
+done
+
