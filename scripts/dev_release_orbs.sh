3f1eef0de5955d460aec58d9c590b12a5f0065f3
ncheneweth <nchenewe@thoughtworks.com>
2018-12-10 21:19:00 -0600

    iterating

--- scripts/dev_release_orbs.sh
+++ scripts/dev_release_orbs.sh
@@ -17,17 +17,9 @@
 #  	exit 1
 #  fi
 #done
-
 for ORB in src/*; do
-    echo >> ${ORB}/orb.yaml
-    orbname=$(basename $ORB)
-    circleci orb source $CIRCLE_PROJECT_USERNAME/$orbname@dev:latest > $orbname.latest
-    diff ${ORB}/orb.yaml $orbname.latest
-#    if [[ $(diff ${ORB}/orb.yaml $orbname.latest) ]]; then
-#    echo "dev-release of $ORB"
-#    # dev release using SHA1 as release version
-#    circleci orb publish ${ORB}/orb.yaml $CIRCLE_PROJECT_USERNAME/$orbname@dev:$CIRCLE_SHA1 --token $CIRCLECI_API_TOKEN
-#    # dev release using 'latest' as release version
-#    circleci orb publish ${ORB}/orb.yaml $CIRCLE_PROJECT_USERNAME/$orbname@dev:latest --token $CIRCLECI_API_TOKEN
-#    fi
+  echo >> ${ORB}/orb.yaml
+  orbname=$(basename $ORB)
+  circleci orb source $CIRCLE_PROJECT_USERNAME/$orbname@dev:latest > $orbname.latest
+  diff ${ORB}/orb.yaml $orbname.latest
 done
