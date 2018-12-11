25cca256d00ad68f72c6938206621be68c506db6
ncheneweth <nchenewe@thoughtworks.com>
2018-12-10 22:16:35 -0600

    updating common-pipeline-tasks

--- scripts/slack_message.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/ash -eo pipefail
-
-curl -X POST -H "Content-type: application/json" --data $1 $SLACK_WEBHOOK
--- tasks.py
+++ tasks.py
@@ -7,3 +7,7 @@ def enc(ctx, file='local.env', encoded_file='env.ci'):
 @task
 def dec(ctx, encoded_file='env.ci', file='local.env'):
     ctx.run("openssl aes-256-cbc -d -in {} -out {} -k $FEEDYARD_CIRCLECI_ENC".format(encoded_file, file))
+
+@task
+def validate(ctx, path):
+    ctx.run('yamllint {}'.format(path))
